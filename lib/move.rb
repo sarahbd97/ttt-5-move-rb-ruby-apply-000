8060c0d40a4c3dce2b919beb1c76604a9fc83cd3
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 03:24:43 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,7 +14,6 @@ def input_to_index(input)
 end
 
 def move(board, index, character = "X")
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   def update_array_at_with(array, index, value)
     array[index] = value
   end
