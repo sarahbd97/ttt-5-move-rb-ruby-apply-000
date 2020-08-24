db5914e5b87a070493b4f2d79f8ee3a08b006876
Sarah Dunford <sarahbd97@gmail.com>
2020-08-24 03:26:48 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -18,6 +18,6 @@ def move(board, index, character = "X")
   def update_array_at_with(array, index, value)
     array[index] = value
   end
-  update_array_at_with(board, index, character)
+  update_array_at_with(board, 0, "X")
   board
 end
